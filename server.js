3a150d9fdd0202841d3790aeeeb83aa81c3e45b2
Dan <danandreiniculae@yahoo.com>
2021-01-19 16:25:32 +0100

    dalsdas

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res)=>
     res.render('index');
 });
 
-app.listen(8080, () => {
-    console.log("Listening on http://localhost:8080");
-});
+// app.listen(8080, () => {
+//     console.log("Listening on http://localhost:8080");
+// });
