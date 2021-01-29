df5cefad4ca5959d36924a5e4c2693a423dd3cd0
Dan <danandreiniculae@yahoo.com>
2021-01-29 22:43:56 +0100

    database added

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ app.use(express.static(path.join(__dirname, "/")));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 app.get('/', (req, res)=>
 {
     res.render('index');
