ddb95441c57cbc5b859ce7736be8475f0cc6c3f4
Dan <danandreiniculae@yahoo.com>
2021-02-01 12:59:04 +0100

    adsda

--- server.js
+++ server.js
@@ -3,30 +3,30 @@ const app = express();
 const path = require('path');
 
 
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-    const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-    });
+//     const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+//     });
 
-    client.connect();
+//     client.connect();
 
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    client.end();
-    });
+//     client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     client.end();
+//     });
 
     
-app.set("/", "html");
-app.use(express.static(path.join(__dirname, "/")));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+// app.set("/", "html");
+// app.use(express.static(path.join(__dirname, "/")));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
 
 
@@ -37,6 +37,6 @@ app.get('/', (req, res)=>
 });
 
 app.listen(process.env.PORT || 80, () => {
-    console.log("Listening on http://visualizr-ar.herokuapp.com:80");
+    console.log("Listening on https://visualizr-ar.herokuapp.com:80");
     
 });
