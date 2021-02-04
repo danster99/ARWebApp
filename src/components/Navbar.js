799cd05d26dd47ac63b29e52e409c15fd4ca6a88
Dan <danandreiniculae@yahoo.com>
2021-02-04 13:31:01 +0200

    navbar.js added

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { AiOutlineEye } from "react-icons/ai";
+import { FaBars,FaTimes } from "react-icons/fa";
+
+function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    return(
+        <>
+            <div className='navbar'>
+                <div className='navbar-container container'>
+                    <Link to='/' className='navbar-logo'>
+                        <AiOutlineEye className='navbar-icon'/>
+                        VISUALIZR
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}/>
+                    {click ? <FaTimes/> : <FaBars/>}
+                </div>
+            </div>
+        </>
+    )
+    
+}
