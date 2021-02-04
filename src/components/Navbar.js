9dd707cd98198b0d51c4f8380c1c52ac373e1dd3
Dan <danandreiniculae@yahoo.com>
2021-02-04 13:38:19 +0200

    state fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from "react-router-dom";
 import { AiOutlineEye } from "react-icons/ai";
 import { FaBars,FaTimes } from "react-icons/fa";
