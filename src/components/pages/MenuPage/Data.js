11ce7abc1ea0499883dc2e86ee92ba5419c468d7
Dan <danandreiniculae@yahoo.com>
2021-06-19 14:02:43 +0300

    added perdele

--- src/components/pages/MenuPage/Data.js
+++ src/components/pages/MenuPage/Data.js
@@ -50,6 +50,11 @@ export const DataLegume = [
     name: 'Oupepaine',    
     description: 'A labracadabradora.',
     price: '30 lei',
+  },
+  {
+    name: 'Perdele',    
+    description: 'A labracadabradora.',
+    price: '30 lei',
   }
   ];
 
