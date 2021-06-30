9a37d2bf7d92ad79525ed327eacc9c18391f6581
Dan Niculae <danandreiniculae@yahoo.com>
2021-06-30 21:22:04 +0300

    added brunch

--- src/components/pages/MenuPage/Data.js
+++ src/components/pages/MenuPage/Data.js
@@ -70,6 +70,11 @@ export const DataLegume = [
     name: 'Micdejunnou',    
     description: 'A labracadabradora.',
     price: '30 lei',
+  },
+  {
+    name: 'Brunch',    
+    description: 'A labracadabradora.',
+    price: '30 lei',
   }
   ];
 
