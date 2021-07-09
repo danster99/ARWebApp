2dbebbfb32df7ddc5f7785bec7a4a4e909935a58
Dan Niculae <danandreiniculae@yahoo.com>
2021-07-09 11:24:50 +0300

    added risotto

--- src/components/pages/MenuPage/Data.js
+++ src/components/pages/MenuPage/Data.js
@@ -75,6 +75,11 @@ export const DataLegume = [
     name: 'Brunch',    
     description: 'A labracadabradora.',
     price: '30 lei',
+  },
+  {
+    name: 'RisottoOk',    
+    description: 'A labracadabradora.',
+    price: '30 lei',
   }
   ];
 
