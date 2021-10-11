a2090b8d84305d8a06ce6242e88104f3c88e7705
Dan Niculae <danandreiniculae@yahoo.com>
2021-10-11 20:41:51 +0300

    changed data.js to new models

--- src/components/pages/MenuPage/Data.js
+++ src/components/pages/MenuPage/Data.js
@@ -1,5 +1,26 @@
 export const client = "Demo";
 
+export async function getCategories(){
+    var categories = [];
+    var response = await fetch('https://test.viar-eu.com/api/api/category/get_all.php')
+      .then(response => response.json());
+    
+    //console.log(response.data);
+
+    response.data.forEach(function(element) {
+      let category ={
+        'name': element.categoryName,
+        'startID':element.startID,
+        'count': element.count
+      }
+      categories.push(category);
+    });
+    console.log(categories);
+    return categories;
+  }
+
+
+
 
 export const EarlyFoods = [
   
@@ -14,14 +35,14 @@ export const EarlyFoods = [
     name: 'Brunch',
     model:'brunch',
     id:'2',    
-    description: 'Clatiat cu spanac, oua, avocado, branza feta, rosii.',
+    description: 'Clatita cu spanac, oua, avocado, branza feta, rosii.',
     price: '25 lei',
   },
   {
     name: 'Bruschetta',
     model:'bruschete',
     id:'3',
-    description: 'Clatiat cu spanac, oua, avocado, branza feta, rosii.',
+    description: 'Bruschete cu rosii, usturoi, busuioc si ulei de masline extra virgin.',
     price: '25 lei',
   }
 ];
@@ -56,14 +77,7 @@ export const MainCourses = [
     model:'sandwich',
     id:'9',
     description: 'test model.',
-    price: '45 lei',
-  },
-  {
-    name: 'Burger Vivo',
-    model:'burger1Vivo',
-    id:'10',
-    description: 'test model.',
-    price: '45 lei',
+    price: '20 lei',
   }
   
 ];
@@ -85,6 +99,54 @@ export const Pastas = [
     price: '28 lei',
   }
 ];
+
+export const Tests = [
+
+  {
+    name: 'Burger Test',
+    model:'burgertest',
+    id:'12',
+    description: 'Paste integrale, guanciale, ou, parmezan.',
+    price: '32 lei',
+  },
+  {
+    name: 'Salata bacon',
+    model:'salatabacon',
+    id:'15',
+    description: 'Paste integrale, sos de rosii, busuioc, parmezan, pui.',
+    price: '28 lei',
+  },{
+    name: 'Burger Test 1',
+    model:'burgertest1',
+    id:'13',
+    description: 'Paste integrale, guanciale, ou, parmezan.',
+    price: '32 lei',
+  },{
+    name: 'Burger Test 2',
+    model:'burgertest2',
+    id:'14',
+    description: 'Paste integrale, guanciale, ou, parmezan.',
+    price: '32 lei',
+  },
+  {
+    name: 'Burger Test 3',
+    model:'burger1Vivo',
+    id:'11',
+    description: 'test model.',
+    price: '45 lei',
+  }
+];
+export const Deserts = [
+
+  {
+    name: 'Cheese cake',
+    model:'prajitura', 
+    id:'10',
+    description: 'Cheese cacke cu gem de capsuni si pudra de cacao.',
+    price: '25 lei',
+  }
+]
+
 
 export const NeaIancu = [
 
@@ -115,6 +177,14 @@ export const NeaIancu = [
       name: 'Main Courses',
       variable: MainCourses
     },
+    {
+      name: 'Deserts',
+      variable: Deserts
+    },
+    {
+      name: 'Tests',
+      variable: Tests
+    }
     // {
     //   name: 'Nenea Iancu',
     //   variable: NeaIancu
