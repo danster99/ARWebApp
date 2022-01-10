6564cc28a4017d997eba546c69ab55904c86f9aa
Dan Niculae <danandreiniculae@yahoo.com>
2022-01-10 21:08:34 +0200

    updated demo website

--- src/components/pages/MenuPage/Data.js
+++ src/components/pages/MenuPage/Data.js
@@ -4,137 +4,47 @@ export const EarlyFoods = [
   
   {
     name: 'Breakfast',
-    model:'micDejun',
     id:'1',   
     description: 'Oua, pita de casa, rosii',
     price: '19 lei',
   },
   {
     name: 'Brunch',
-    model:'brunch',
     id:'2',    
     description: 'Clatita cu spanac, oua, avocado, branza feta, rosii.',
     price: '25 lei',
-  },
+  }
+];
+
+export const Burgers = [
+
   {
-    name: 'Bruschetta',
-    model:'bruschete',
+    name: 'All in Burger',
     id:'3',
-    description: 'Bruschete cu rosii, usturoi, busuioc si ulei de masline extra virgin.',
-    price: '25 lei',
-  }
-];
-
-export const Pizzas = [ 
+    description: 'Paine brioche, ketchup, carne vita extra gramaj, bacon, parmezan, branza facuta in house, ulei de trufe, ceapa caramelizata, salata verde, rosii, castraveti murati. (Alergeni: gluten oua, lapte, susan, mustar) 400gr',
+    price: '35 lei',
+  },
   {
-    name: 'Pizza Super Suprema',
-    model:'pizzaSuprema',
+    name: 'Clasic Burger',
     id:'4',
-    description: 'Blat pufos, crema de branza, sos de roșii, mozzarela, vita, cipuerci',
-    price: '32 lei',
-  }
-];
-
-export const MainCourses = [ 
-  {
-    name: 'Steak & wedges',   
-    model:'friptura',
-    id:'7',
-    description: 'Vita Black Angus, rozmarin, cartofi.',
-    price: '69 lei',
+    description: 'Paine brioche, ketchup, salata verde, carne, rosii, ceapa rosie, castraveti murati. (Alergeni: gluten, oua, lapte, susan) 300gr',
+    price: '30 lei',
   },
   {
-    name: 'Burger',
-    model:'burger',
-    id:'8',
-    description: 'Chifla de casa, carne de vita, cartofi, rosie, ceapa, salata.',
-    price: '45 lei',
-  },
-  {
-    name: 'Sandwich',
-    model:'sandwich',
-    id:'9',
-    description: 'test model.',
-    price: '20 lei',
-  }
-  
-];
-
-export const Pastas = [
-
-  {
-    name: 'Pasta carbonara',
-    model:'carbonara',
+    name: 'Fried Chicken Burger',
     id:'5',
-    description: 'Paste integrale, guanciale, ou, parmezan.',
-    price: '32 lei',
-  },
-  {
-    name: 'Pasta arrabiata',
-    model:'arrabiata',
-    id:'6',
-    description: 'Paste integrale, sos de rosii, busuioc, parmezan, pui.',
-    price: '28 lei',
-  }
-];
-
-export const Tests = [
-
-  {
-    name: 'Burger Test',
-    model:'burger1Vivo',
-    id:'11',
-    description: 'test model.',
-    price: '45 lei',
-  },
-  {
-    name: 'Burger Test 1',
-    model:'burgertest',
-    id:'12',
-    description: 'Paste integrale, guanciale, ou, parmezan.',
-    price: '32 lei',
-  },
-  {
-    name: 'Burger Test 2',
-    model:'burgertest1',
-    id:'13',
-    description: 'Paste integrale, guanciale, ou, parmezan.',
-    price: '32 lei',
-  },
-  {
-    name: 'Burger Test 3',
-    model:'burgertest2',
-    id:'14',
-    description: 'Paste integrale, guanciale, ou, parmezan.',
-    price: '32 lei',
-  },
-  {
-    name: 'Salata bacon',
-    model:'salatabacon',
-    id:'15',
-    description: 'Paste integrale, sos de rosii, busuioc, parmezan, pui.',
-    price: '28 lei',
+    description: 'Paine brioche, salata coleslaw, crispy pui, branza Gouda , castraveti murati, sos aioli. (Alergeni: gluten, oua, lapte, susan,mustar) 350gr',
+    price: '25 lei',
   }
 ];
 export const Deserts = [
 
   {
     name: 'Cheese cake',
-    model:'prajitura', 
-    id:'10',
+    model:'cheesecake', 
+    id:'6',
     description: 'Cheese cacke cu gem de capsuni si pudra de cacao.',
     price: '25 lei',
-  }
-]
-
-
-export const NeaIancu = [
-
-  {
-    name: 'Platou Catavencu',
-    model:'platou-catavencu', 
-    description: 'Mici, carnaciori picanti, pastrama de pui, Cartofiori, muraturi, ketchup, mustar.',
-    price: '30 lei',
   }
 ]
 
@@ -146,27 +56,11 @@ export const NeaIancu = [
       variable: EarlyFoods
     },
     {
-      name: 'Pizza',
-      variable: Pizzas
-    },
-    {
-      name: 'Pasta',
-      variable: Pastas
-    },
-    {
-      name: 'Main Courses',
-      variable: MainCourses
+      name: 'Burgers',
+      variable: Burgers
     },
     {
       name: 'Deserts',
       variable: Deserts
-    },
-    {
-      name: 'Tests',
-      variable: Tests
     }
-    // {
-    //   name: 'Nenea Iancu',
-    //   variable: NeaIancu
-    // }
   ];
